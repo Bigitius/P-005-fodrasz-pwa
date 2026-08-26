a494e90c99f6573becd8198f0457426280dcd297
Bigitius <egervari.i@gmail.com>
2026-08-26 18:24:57 +0200

    Offline mukodes javitasa: skipWaiting + clients.claim() a service workerben (v16)

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-﻿const CACHE_NAME = 'fodrasz-foglalo-v15';
+﻿const CACHE_NAME = 'fodrasz-foglalo-v16';
 const CORE_ASSETS = ['./', './index.html', './manifest.json', './service-worker.js', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
   );
@@ -11,7 +12,7 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
