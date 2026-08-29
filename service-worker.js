685f00fb36f07b93785733e414c91fb80594484c
Bigitius <egervari.i@gmail.com>
2026-08-29 12:04:53 +0200

    Offline navigacio megbizhatobba tetele: navigate mod eseten mindig cache-elt index.html-t adjunk vissza (v17)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'fodrasz-foglalo-v16';
+﻿const CACHE_NAME = 'fodrasz-foglalo-v17';
 const CORE_ASSETS = ['./', './index.html', './manifest.json', './service-worker.js', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
@@ -17,6 +17,16 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  // Oldal-navigacio (pl. az ikonrol inditaskor, vagy ujratoltes): mindig
+  // probaljuk elobb a halozatot (friss tartalom), de ha nincs net, biztosan
+  // adjuk vissza a gyorsitotarazott foldalt -- ne a pontos URL-egyezesre
+  // hagyatkozzunk, mert atiranyitas/parameter miatt eltero URL is lehet.
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
   event.respondWith(
     caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
